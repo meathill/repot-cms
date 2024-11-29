be6ad5aa25d79b9724e86e70bb345a93a2a1cb2a
Meathill <meathill@gmail.com>
2024-11-29 22:29:46 +0800

    bug: more options

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,9 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  proxy: true,
+  proxy: {
+    koa: true,
+  },
   app: {
     keys: env.array('APP_KEYS'),
   },
